bd8218f1311ff5e9fa30c4a0f070933ff14d10f5
Ekaterina <ekaterina.s.velichko@gmail.com>
2022-06-10 14:20:26 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2: Пользователь вводит с клавиатуры кол-во строк и столбцов для массива. 
+//Вывести массив в виде матрицы, создав "рамку" из единиц, а внутреннюю зону матрицы заполнить нулями.
+// Пример: 3, 4 ->
+//  1 1 1 1
+//  1 0 0 1
+//  1 1 1 1
+
+Console.WriteLine("Введите количество строк для массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество стoлбцов для массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+         if (i == 0 || j == 0 || i == array.GetLength(0)-1 || j == array.GetLength(1)-1 )   array[i, j] = 1;
+	     else array[i, j] = 0;
+         Console.Write(array[i, j] + " ");
+     }
+     Console.WriteLine(" ");
+}     
